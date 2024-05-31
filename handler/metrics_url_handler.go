4cebe270054a66397608d649f4339e7229c98702
Vighnesh Pol <vigupol@gmail.com>
2024-05-31 12:57:46 +0530

    Add top 3 domain counts to metrics API response

--- handler/metrics_url_handler.go
+++ handler/metrics_url_handler.go
@@ -1,22 +1,26 @@
 package handler
 
 import (
+	"encoding/json"
 	"go-url-shortener-service/storage"
 	"net/http"
-	"encoding/json"
 )
 
 // TopDomainsHandler returns a handler function that retrieves the top domains
 func TopDomainsHandler(urlStore *storage.URLStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		//method to retrieve the top 3 domains with the highest counts
 		topDomains := urlStore.GetTopDomains(3)
+		//domainCounts map to store the counts for the top 3 domains
 		domainCounts := make(map[string]int)
 
+		//Iterating over the topDomains slice and updating the domainCounts map with the counts for each domain
 		for _, domain := range topDomains {
 			domainCounts[domain] = urlStore.DomainCounts()[domain]
 		}
 
 		w.Header().Set("Content-Type", "application/json")
+		//Encoding the domainCounts map as JSON and sending it as the response
 		json.NewEncoder(w).Encode(domainCounts)
 	}
 }
